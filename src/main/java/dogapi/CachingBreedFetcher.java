5a4c1c8d23b731b2534156e41182512e078d0b5d
Julia <julia.teixeira@mail.utoronto.ca>
2025-10-19 18:58:14 -0400

    got rid of TODOs

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -13,7 +13,6 @@ import java.util.*;
  * The cache maps the name of a breed to its list of sub breed names.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
     private final BreedFetcher fetcher; // the real fetcher (like DogApiBreedFetcher)
     private final Map<String, List<String>> cache = new HashMap<>(); // store results here
     private int callsMade = 0;
--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -13,7 +13,7 @@ public class Main {
         breed = "cat";
         result = getNumberOfSubBreeds(breed, breedFetcher);
         System.out.println(breed + " has " + result + " sub breeds");
-        System.out.println(breedFetcher.getSubBreeds("hound"));
+        System.out.println(breedFetcher.getSubBreeds("cat"));
     }
 
     /**
@@ -25,8 +25,6 @@ public class Main {
      * returned by the fetcher
      */
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
-        // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
-        // return statement included so that the starter code can compile and run.
         try {
             List<String> subBreeds = breedFetcher.getSubBreeds(breed);
             return subBreeds.size();
